8f94b9766ab3f5750715dee61c7bdcdf1ac24258
Zach Larsen <zacharyjlarsen@gmail.com>
2022-06-20 14:23:52 +0100

    basetemplate started

--- TimetableLab/Timetabler/urls.py
+++ TimetableLab/Timetabler/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('teachers', views.teachers, name='teachers'),
 ]
--- TimetableLab/Timetabler/views.py
+++ TimetableLab/Timetabler/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "index.html")
+
+def teachers(request):
+    return render(request, "teachers.html")
